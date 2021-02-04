f47a9a1c01271ffb8a7923de1a4fa183e2f90441
VitalieVSV <dut66AHJCT>
2021-02-04 20:40:03 +0200

    ThirdTask.java, solved two ways

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,17 @@
 import tasks.FirstTask;
 import tasks.SecondTask;
+import tasks.ThirdTask;
 
 public class Main {
     public static void main(String[] args) {
-        FirstTask firstTask = new FirstTask();
+        /*FirstTask firstTask = new FirstTask();
         firstTask.solve();
 
         SecondTask secondTask = new SecondTask();
-        secondTask.solve();
+        secondTask.solve();*/
 
+        ThirdTask thirdTask = new ThirdTask();
+        thirdTask.solveStringVersion();
 
     }
 }
